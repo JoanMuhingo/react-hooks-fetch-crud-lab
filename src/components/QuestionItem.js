7f72a0e05988d96f13de9b1a90f4ccc5765c37fc
Joan <joanmuhingo@gmail.com>
2023-12-18 13:34:36 +0300

    progress

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,23 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question ,onDelete ,onUpdate}) {
   const { id, prompt, answers, correctIndex } = question;
+  const [selectedCorrectIndex, setSelectedCorrectIndex] =useState(correctIndex);
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
       {answer}
     </option>
   ));
+  const handleDelete= () => {
+    onDelete(id);
+  }
+ const handleUpdate =(e) => {
+  const newCorrectIndex = parseInt(e.target.value, 10);
 
+  setSelectedCorrectIndex(newCorrectIndex);
+
+  fetch(`http://localhost:4000/questions/${id}`,{
+    method:'PATCH',
+    headers:{
+      'Content-Type': "application/json",
+    },
+    body:JSON.stringify({
+      correctIndex: newCorrectIndex,
+    }),
+  })
+  .then((r) => r.json())
+  .then(updateQuestion =>{
+    onUpdate(updateQuestion.id, newCorrectIndex);
+  })
+ }
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleUpdate}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,11 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
+import QuestionForm from "./QuestionForm";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+      .then((r) => r.json())
+      .then((questions) => setQuestions(questions));
+  }, []);
+
+  const addQuestion = (newQuestion) => {
+    setQuestions((prevQuestions) => [...prevQuestions, newQuestion]);
+  };
+
+  const handleDelete = (questionId) => {
+    setQuestions((prevQuestions) =>
+      prevQuestions.filter((question) => question.id !== questionId)
+    );
+
+    fetch(`http://localhost:4000/questions/${questionId}`, {
+      method: 'DELETE',
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        setQuestions((prevQuestions) =>
+          prevQuestions.filter((question) => question.id !== questionId)
+        );
+      });
+  };
+
+  const handleUpdate = (questionId, newCorrectIndex) => {
+    setQuestions((prevQuestions) =>
+      prevQuestions.map((question) => {
+        if (question.id === questionId) {
+          return {
+            ...question,
+            correctIndex: newCorrectIndex,
+          };
+        }
+        return question;
+      })
+    );
+  };
+
   return (
-    <section>
-      <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
+    <div>
+      <section>
+        <h1>Quiz Questions</h1>
+        <ul>
+          {questions.map((question) => (
+            <QuestionItem
+              key={question.id}
+              question={question}
+              onDelete={handleDelete}
+              onUpdate={handleUpdate}
+            />
+          ))}
+        </ul>
+      </section>
+      <QuestionForm addQuestion={addQuestion} />
+    </div>
   );
 }
 
